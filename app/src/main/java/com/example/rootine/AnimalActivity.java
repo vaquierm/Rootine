0233f4950a60791cdae48259380435271d9cf9b4
Alison Caulfield <arcaulfield@gmail.com>
2020-01-18 12:46:29 -0500

    minor changes

--- app/src/main/java/com/example/rootine/AnimalActivity.java
+++ app/src/main/java/com/example/rootine/AnimalActivity.java
@@ -2,12 +2,26 @@ package com.example.rootine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class AnimalActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        //create layout
+        recyclerView.setHasFixedSize(true);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
     }
 }
