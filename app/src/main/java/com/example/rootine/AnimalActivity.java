7b2e9638c6afdb507953c415d34083bf2143e6ac
Alison Caulfield <arcaulfield@gmail.com>
2020-01-18 14:22:26 -0500

    made progress on recycler view

--- app/src/main/java/com/example/rootine/AnimalActivity.java
+++ app/src/main/java/com/example/rootine/AnimalActivity.java
@@ -2,17 +2,36 @@ package com.example.rootine;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class AnimalActivity extends AppCompatActivity {
 
+    private static final String TAG = "AnimalActivity";
+
+    //vars
+    private ArrayList<String> animalNames = new ArrayList<>();
+    private ArrayList<String> animalImages = new ArrayList<>();
     RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal);
+        Log.d(TAG, "onCreate: started");
+
+
+        initImageBitmaps();
+
+
+
+
+
 
         recyclerView = findViewById(R.id.recyclerView);
 
@@ -23,5 +42,24 @@ public class AnimalActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
 
 
+    }
+
+    private void initImageBitmaps(){
+        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+
+        animalImages.add("");
+        animalNames.add("cat");
+
+        initRecyclerView();
+
+        //add images to thing
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(animalNames, animalNames, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/rootine/RecyclerViewAdapter.java
@@ -0,0 +1,70 @@
+package com.example.rootine;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImageNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> imageNames, ArrayList<String> images, Context context){
+        mImageNames = imageNames;
+        mImages = images;
+        mContext = context;
+    }
+
+    //responsible for inflating the view
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+        ///figure out how to update the images as well!!!
+        holder.imageName.setText(mImageNames.get(position));
+        holder.parentLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+               //Log.d(TAG, "onClick: clicked on: " + mImageNames.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageNames.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView imageName;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.image);
+            imageName = itemView.findViewById(R.id.imageName);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
